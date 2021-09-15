c195b9fe38fbea28aa2d92080444f5764acd8bdb
krasnikovra <roma_krasnikov02@mail.ru>
2021-09-15 15:39:41 +0300

    Developed financies query for year

--- src/db_financies.py
+++ src/db_financies.py
@@ -25,7 +25,9 @@ def db_financies_day(date):
         rows = cur.fetchall()
         return rows
 
+
 # Returns OrderId, Orders.AbonementId, RoomName, ServiceName, TimeName, OrderDays, OrderPrice
+# date in mm.YYYY
 def db_financies_month(date):
     # Connect to the local MySQL 'Fitness' db
     con = pymysql.connect(host='127.0.0.1',
@@ -50,5 +52,32 @@ def db_financies_month(date):
         rows = cur.fetchall()
         return rows
 
+
+# Returns OrderId, Orders.AbonementId, RoomName, ServiceName, TimeName, OrderDays, OrderPrice
+# date in YYYY
+def db_financies_year(date):
+    # Connect to the local MySQL 'Fitness' db
+    con = pymysql.connect(host='127.0.0.1',
+                          user='root',
+                          password='gpnb0b',
+                          db='Fitness',
+                          charset='utf8mb4')
+
+    with con:
+        cur = con.cursor()
+        cur.execute('SELECT OrderId, Orders.AbonementId, RoomName, ServiceName, TimeName, '
+                    'DATE_FORMAT(OrderDateStart, "%d.%m.%Y"), DATEDIFF(OrderDateEnd, OrderDateStart) + 1, '
+                    'AbonementPrice * (DATEDIFF(OrderDateEnd, OrderDateStart) + 1) '
+                    'FROM Orders '
+                    'INNER JOIN Abonements ON Abonements.AbonementId = Orders.AbonementId '
+                    'INNER JOIN Rooms ON Rooms.RoomId = Abonements.RoomId '
+                    'INNER JOIN Services ON Services.ServiceId = Abonements.ServiceId '
+                    'INNER JOIN Times ON Times.TimeId = Abonements.TimeId '
+                    'WHERE YEAR(STR_TO_DATE("{}", "%m.%Y")) = YEAR(OrderDateStart) '
+                    'ORDER BY OrderId'.format(date))
+        rows = cur.fetchall()
+        return rows
+
+
 if __name__ == '__main__':
-    rows = db_financies_month('09.2021')
+    rows = db_financies_year('2021')
